1bfe24775b148095479580db4df8ff2bf74dd23f
Leonardo Lopes <l.lopes355@gmail.com>
2022-08-20 22:18:24 -0300

    Bloqueio dos campos e botão voltar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImg from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
 import { levels, calculateImc, Level } from './helpers/imc';
 import { GridItem } from './components/GridItem';
 
@@ -15,6 +16,12 @@ const App = () => {
     } else {
       alert("Digite todos os campos.");
     }
+  }
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   }
 
   return (
@@ -34,15 +41,17 @@ const App = () => {
             placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
             value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))} 
+            disabled={toShow ? true : false}
           />
           <input 
             type="number"
             placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightField(parseFloat(e.target.value))} 
+            disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button onClick={handleCalculateButton } disabled={toShow ? true : false}>Calcular</button>
         </div>
         <div className={styles.rightside}>
           {!toShow && 
@@ -54,7 +63,9 @@ const App = () => {
           }
           {toShow &&
             <div className={styles.rightBig}>
-              <div className={styles.rightArrow}></div>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
               <GridItem item={toShow} />
             </div>
           }
